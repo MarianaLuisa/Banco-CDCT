b7a1b8bf4ae2ed82dca08aa847b4719a5360ddec
Mariana Luísa <144719154+MarianaLuisa@users.noreply.github.com>
2024-02-22 11:46:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+// Adiciona um evento de teclado ao botão de login
+document.getElementById('loginForm').addEventListener('keyup', function(event) {
+  // Verifica se a tecla pressionada é Enter (código 13)
+  if (event.key === 'Enter') {
+    // Chama a função de login quando Enter é pressionado
+    login();
+  }
+});
+
+function login() {
+  const usuario= document.getElementById("usuario").value;
+  const senha= document.getElementById("senha").value;
+
+  /*Outra forma de mandar mensagem de usuário e senha incorretos*/
+  /*const mensagemErro = document.getElementById("mensagemErro");*/
+
+  if (usuario == "bioinfo" && senha == "12345"){
+    /*alert("Sucesso");*/
+    localStorage.setItem("usuarioLogado", true);
+    location.href = "home.html";
+  }
+  else {
+    alert("Usuário ou senha incorretos");
+
+    /*Outra forma de mandar mensagem de usuário e senha incorretos*/
+    /*mensagemErro.textContent = "Usuário ou senha incorretos";*/
+
+    document.getElementById('usuario').value='';
+    document.getElementById('senha').value='';
+  }
+}
+// Limpar os campos quando a página for carregada ou restaurada do cache
+window.addEventListener('pageshow', function(event) {
+  var historyTraversal = event.persisted || (typeof window.performance != 'undefined' && window.performance.navigation.type === 2);
+  if (historyTraversal) {
+    // A página está sendo carregada do cache, então limpe os campos
+    document.getElementById('usuario').value = '';
+    document.getElementById('senha').value = '';
+  }
+});
